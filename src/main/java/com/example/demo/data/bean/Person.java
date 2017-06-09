01add2e22aa8d87890b7218d8f34230ade3190b4
wangjun.shi <shiwangjun1@gmail.com>
2017-06-09 23:43:35 +0200

    resolved warnings

--- src/main/java/com/example/demo/data/bean/Person.java
+++ src/main/java/com/example/demo/data/bean/Person.java
@@ -6,9 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
